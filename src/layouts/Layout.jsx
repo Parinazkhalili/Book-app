0818e965952db22b3a098429bd3acfbb0d50dfe3
Parinaz KHalili <parinazkhalili27@gmail.com>
2024-03-02 19:18:01 +0330

    layout file added to my project

---
+++ src/layouts/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react' ;
+
+
+function Layout({children}) {
+    return (
+       <>
+        <header>header</header>
+        {children}
+        <footer> footer</footer>
+       </> 
+    )
+}
+
+export default Layout
