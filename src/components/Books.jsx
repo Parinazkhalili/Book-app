ed67c22d4a03f0c0ca23334e911ee25ca2a46270
Parinaz KHalili <parinazkhalili27@gmail.com>
2024-03-07 11:48:45 +0330

    Added component search to project

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,13 +1,17 @@
 
-import {books} from "../constants/mockData"
-import React, { useState } from 'react'
+import {books as bookData} from "../constants/mockData"
+import React, { useState, useSyncExternalStore } from 'react'
 import BookCard from "./BookCard"
 import SideCard from "./SideCard";
 
 import styles from "./Books.module.css"
+import SearchBox from "./SearchBox";
 
 function Books() {
     const [liked , setLiked] =useState([]) ;
+
+    const [search , setSearch] =useState ([]) ;
+    const [books, setBooks] = useState (bookData)
 
     const handleLikedList = (book , status) =>{
        
@@ -20,7 +24,25 @@ function Books() {
             setLiked((liked) => [...liked , book]) ;
         } 
     }
+
+     const searchHandler = () => {
+        if (search) {
+            const newBooks = bookData.filter ((book) =>book.title.toLocaleLowerCase().includes(search)
+            );
+            setBooks(newBooks)
+        } else {
+            setBooks(bookData);
+        }
+        
+     }
+
+
     return (
+
+        <>
+
+
+        <SearchBox  search={search} setSearch={setSearch} searchHandler={searchHandler}/>
         <div  className={styles.container}>
             <div className={styles.cards}>
                 {books.map((book) => (<BookCard  key={book.id} data={book} handleLikedList={handleLikedList}/>))}</div>
@@ -28,6 +50,7 @@ function Books() {
                 <h4>Favorites</h4>
                 {liked.map (book => <SideCard  key={book.id} data={book}/>)}</div>}
         </div>
+        </>
     )
 }
 
